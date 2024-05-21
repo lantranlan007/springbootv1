2927018a69c43ab99d5374898ce5c6f6bd103313
lantranlan007 <71231001+lantranlan007@users.noreply.github.com>
2024-05-21 21:47:47 +0700

    Add more

---
+++ clientoall/src/main/java/com/datvu/clientoall/ClientoallApplication.java
@@ -0,0 +1,13 @@
+package com.datvu.clientoall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientoallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientoallApplication.class, args);
+	}
+
+}
---
+++ clientoall/src/test/java/com/datvu/clientoall/ClientoallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datvu.clientoall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientoallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
